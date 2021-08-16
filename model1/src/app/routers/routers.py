853a18facde25c267ee5448417d61072f9a89004
kooose38 <kooose0308@gmail.com>
2021-08-16 15:03:29 +0900

    create model1

--- model1/src/app/routers/routers.py
+++ model1/src/app/routers/routers.py
@@ -1,11 +1,11 @@
-import uuid
 from logging import getLogger
 from typing import Any, Dict, List
 import time 
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException 
 from src.ml.prediction import Data, classifier
 from src.ml.outlier_detection import outlier_detector
+from src.utils.profiler import log_decorator
 
 logger = getLogger(__name__)
 router = APIRouter()
@@ -30,38 +30,17 @@ def metadata() -> Dict[str, Any]:
     }
 
 
+##########################################################################
 @router.get("/label")
 def label() -> Dict[int, str]:
     return classifier.label
 
 
-@router.get("/predict/test")
-def predict_test() -> Dict[str, Any]:
-    result = _predict(Data(), "TEST", True)
-    return result 
-
-@router.get("/predict/test/label")
-def predict_test_label() -> Dict[str, Any]:
-    result = _predict(Data(), "TEST", False)
-    return result 
-
-@router.post("/predict")
-def predict(data: Data, job_id: str) -> Dict[str, Any]:
-    result = _predict(data, job_id, True)
-    return result 
-
-@router.post("/predict/label")
-def predict_label(data: Data, job_id: str) -> Dict[str, Any]:
-    result = _predict(data, job_id, False)
-    return result 
-
-
-def _predict(data: Data, job_id: str, flg: bool) -> Dict[str, Any]:
+##########################################################################
+@log_decorator(endpoint="/predict/test", logger=logger)
+def _predict_test(data: Data, job_id: str) -> Dict[str, Any]:
     start_class = time.time()
-    if flg:
-        pred = classifier.predict(data.data)
-    else:
-        pred = classifier.predict_label(data.data)
+    pred: List[float] = classifier.predict(data.data)
     end_elapsed = 1000 * (time.time() - start_class)
 
     start_outlier = time.time()
@@ -77,4 +56,99 @@ def _predict(data: Data, job_id: str, flg: bool) -> Dict[str, Any]:
         "is_outlier": is_outlier,
         "outlier_elapsed": outlier_elapsed
     }
+
+@router.get("/predict/test")
+def predict_test() -> Dict[str, Any]:
+    result = _predict_test(Data(), "TEST")
+    return result 
+
+
+##########################################################################
+@log_decorator(endpoint="/predict/test/label", logger=logger)
+def _predict_test_label(data: Data, job_id: str) -> Dict[str, Any]:
+    start_class = time.time()
+    pred: List[float] = classifier.predict_label(data.data)
+    end_elapsed = 1000 * (time.time() - start_class)
+
+    start_outlier = time.time()
+    is_outlier, outlier_scoer = outlier_detector.predict(data.data)
+    outlier_elapsed = 1000 * (time.time() - start_outlier)
+
+    return {
+        "job_id": job_id,
+        "data": data.data,
+        "prediction": pred,
+        "prediction_elapsed": end_elapsed,
+        "outlier_score": outlier_scoer,
+        "is_outlier": is_outlier,
+        "outlier_elapsed": outlier_elapsed
+    }
+
+@router.get("/predict/test/label")
+def predict_test_label() -> Dict[str, Any]:
+    result = _predict_test_label(Data(), "TEST")
+    return result 
+
+
+##########################################################################
+@log_decorator(endpoint="/predict", logger=logger)
+def _predict(data: Data, job_id: str) -> Dict[str, Any]:
+    if len(data.data) != 1 or len(data.data[0]) != 4:
+        raise HTTPException(status_code=404, detail="invalid input data")
+
+    start_class = time.time()
+    pred: List[float] = classifier.predict(data.data)
+    end_elapsed = 1000 * (time.time() - start_class)
+
+    start_outlier = time.time()
+    is_outlier, outlier_scoer = outlier_detector.predict(data.data)
+    outlier_elapsed = 1000 * (time.time() - start_outlier)
+
+    return {
+        "job_id": job_id,
+        "data": data.data,
+        "prediction": pred,
+        "prediction_elapsed": end_elapsed,
+        "outlier_score": outlier_scoer,
+        "is_outlier": is_outlier,
+        "outlier_elapsed": outlier_elapsed
+    }
+
+@router.post("/predict")
+def predict(data: Data, job_id: str) -> Dict[str, Any]:
+    result = _predict(data, job_id)
+    return result 
+
+
+##########################################################################
+@log_decorator(endpoint="/predict/label", logger=logger)
+def _predict_label(data: Data, job_id: str) -> Dict[str, Any]:
+    if len(data.data) != 1 or len(data.data[0]) != 4:
+        raise HTTPException(status_code=404, detail="invalid input data")
+        
+    start_class = time.time()
+    pred: str = classifier.predict_label(data.data)
+    end_elapsed = 1000 * (time.time() - start_class)
+
+    start_outlier = time.time()
+    is_outlier, outlier_scoer = outlier_detector.predict(data.data)
+    outlier_elapsed = 1000 * (time.time() - start_outlier)
+
+    return {
+        "job_id": job_id,
+        "data": data.data,
+        "prediction": pred,
+        "prediction_elapsed": end_elapsed,
+        "outlier_score": outlier_scoer,
+        "is_outlier": is_outlier,
+        "outlier_elapsed": outlier_elapsed
+    }
+    
+
+@router.post("/predict/label")
+def predict_label(data: Data, job_id: str) -> Dict[str, Any]:
+    result = _predict_label(data, job_id)
+    return result 
+
+##########################################################################
     
